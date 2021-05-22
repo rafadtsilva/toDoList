3f6400352a608863e381494e63974fcd70de30e7
Rafael Trindade <rafaeltrindade@outlook.com>
2021-05-22 11:08:31 -0300

    fase 5

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+onload = function () {
+    let task = localStorage.getItem("task");
+    let title = document.getElementById("task")
+}
+
+function inserir(e) {
+    let valor = e.value;
+    console.log(e);
+    
+    // let title = document.getElementById("task");
+    // title.innerHTML = valor;
+
+    // localStorage.setItem("task", valor);
+}

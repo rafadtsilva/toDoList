10bfb3fcca9b4b8e47030f21e7fc5498178cf997
Rafael Trindade <rafaeltrindade@outlook.com>
2021-06-03 15:45:59 -0300

    fase 7

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,13 @@ onload = function () {
     let title = document.getElementById("task")
 }
 
-function inserir(e) {
+function inserir(x, e) {
     let valor = e.value;
     console.log(valor);
     e.value = "";
+    let inp = ["d1", "d2", "d3", "d4", "d5", "d6", "d7"];
 
-    let v1 = document.getElementById("d1");
+    let v1 = document.getElementById(inp[x]);
     console.log(v1.innerHTML);
     v1.innerHTML = v1.innerHTML + "<p>" + valor + "</p>";
     
